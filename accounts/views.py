8aae69f7b7fa1e7eb0d971096c94b8ce00f1867e
Tushar Muley <tusharmule232@gmail.com>
2025-12-29 15:16:26 +0530

    changes password status codes aded

--- accounts/views.py
+++ accounts/views.py
@@ -453,18 +453,18 @@ class ChangePasswordView(APIView):
             new_password = request.data.get("new_password")
             confirm_password = request.data.get("confirm_password")
             if not old_password or not new_password or not confirm_password:
-                return Response({"message": "All fields are required"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "All fields are required", "status": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
             if new_password != confirm_password:
-                return Response({"message": "New password and confirm password do not match"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "New password and confirm password do not match", "status": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
             if not old_password or not new_password:
-                return Response({"message": "Old password and new password are required"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "Old password and new password are required", "status": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
             if not user.check_password(old_password):
-                return Response({"message": "Old password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "Old password is incorrect", "status": status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
             user.set_password(new_password)
             user.save()
             return Response({"message": "Password changed successfully", "status": status.HTTP_200_OK}, status=status.HTTP_200_OK)
         except Exception as e:
             line_number = sys.exc_info()[2].tb_lineno
-            return Response({"message": "Something went wrong", "error": str(e), "line_number": line_number },status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"message": "Something went wrong", "error": str(e), "line_number": line_number, "status": status.HTTP_500_INTERNAL_SERVER_ERROR}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
         
