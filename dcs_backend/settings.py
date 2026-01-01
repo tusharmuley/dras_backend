d88adbccc277492e5d1b8e723dfe57d8a7dc9720
Gaurav Soni <gaurav.soni@quantiantech.com>
2026-01-01 12:33:51 +0530

    modified setting file

--- dcs_backend/settings.py
+++ dcs_backend/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'dcs_backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "dcs"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
